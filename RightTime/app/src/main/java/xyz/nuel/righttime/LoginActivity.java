bea225f501f581f20ff516ce28fa7c8537b279d5
sebastian cedeño gonzález <bastiangaxiola@gmail.com>
2019-05-07 17:54:37 -0500

    Uyx

--- RightTime/app/src/main/java/xyz/nuel/righttime/LoginActivity.java
+++ RightTime/app/src/main/java/xyz/nuel/righttime/LoginActivity.java
@@ -104,6 +104,7 @@ public class LoginActivity extends AppCompatActivity {
             }
 
             @Override
+
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
